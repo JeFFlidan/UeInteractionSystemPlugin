b5fbfd35017a1c02ba5331a2a3f328560bb1f2ea
JeFFlidan <zaveruha678@gmail.com>
2024-10-02 20:39:55 +0300

    Added Data Table row structure

---
+++ Source/InteractionSystem/Public/Core/InteractableData.h
@@ -0,0 +1,24 @@
+﻿// Copyright Kyrylo Zaverukha. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "InteractableData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FInteractableData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "User Interface")
+	FText DisplayText{FText::GetEmpty()};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "User Interface")
+	FText DescriptionText{FText::GetEmpty()};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+	float DetectionDistance{250.0f};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+	float InteractionDuration{0.0f};
+};
